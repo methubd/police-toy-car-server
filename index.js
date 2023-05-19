320967ebae623670881404cff71b9970dcfd4eb9
T R Methu <fmethubd@gmail.com>
2023-05-19 10:05:55 +0600

    readed data by email for my toy page

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
     await client.connect();
 
     const toysCollection = client.db('PliceToysDB').collection('toys');
+    const myToysCollection = client.db('PliceToysDB').collection('myToys');
 
     // Reading 'Ambulance' category
     app.get('/ambulance', async (req, res) => {
@@ -54,15 +55,30 @@ async function run() {
     app.get('/toys', async (req, res) => {
         const result = await toysCollection.find().toArray();
         res.send(result)
-    })
-
-    
+    })    
     
     // Toy by Id 
     app.get('/toys/:id', async (req, res) => {
         const id = req.params.id;
         const query = {_id: new ObjectId(id)}
         const result = await toysCollection.findOne(query)
+        res.send(result);
+    })
+
+    //Reading MyToy by email
+    app.get('/myToys', async (req, res) => {
+      let query = {}
+      if(req.query.email){
+        query = {userEmail: req.query.email}
+      }
+      const result = await myToysCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    // My Toy Routes (POST)
+    app.post('/myToys', async (req, res) => {
+        const newToy = req.body;
+        const result = await myToysCollection.insertOne(newToy)
         res.send(result);
     })
 
