4b3d33638d6aa7449ff0e71758b88d64be1f3262
T R Methu <fmethubd@gmail.com>
2023-05-20 15:51:09 +0600

    a link edited

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// https://assignment-11-server-methubd.vercel.app
-
 //middleware
 app.use(cors());
 app.use(express.json());
